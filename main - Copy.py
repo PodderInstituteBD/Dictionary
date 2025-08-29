4434b93f3459613ffe867cab15c73cd7ada08b98
Shreejit Podder <Shreejitpodder187@gmail.com>
2025-08-29 21:38:28 +0600

    Add files via upload

---
+++ main - Copy.py
@@ -0,0 +1,46 @@
+studentdata={'id1':
+
+{'name': ['Sara'],
+
+'class': ['V'],
+
+'subject_integration': ['english, math, science']
+
+},
+
+'id2':
+
+{'name': ['David'],
+
+'class': ['V'],
+
+'subject_integration': ['english, math, science']
+
+},
+
+'id3':
+
+{'name': ['Sara'],
+
+'class': ['V'],
+
+'subject_integration': ['english, math, science']
+
+},
+
+'id4':
+
+{'name': ['Surya'],
+
+'class': ['V'],
+
+'subject_integration': ['english, math, science']
+
+},
+
+}
+result={}
+for key , value in studentdata.items():
+    if value not in result.values():
+        result [key]=value
+print(result)
---
+++ main2 - Copy.py
@@ -0,0 +1,4 @@
+country_code={'india':"0091","australia":"0025","nepal":"00977"}
+
+print(country_code.get("india","not founde"))
+print(country_code.get("japan","not founde"))
